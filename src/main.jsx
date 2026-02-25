437a379378323c8929d9f4bfd3093872794d8edf
Paola Di Maio <Starborn@users.noreply.github.com>
2026-02-25 14:46:08 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,11 @@ import Home from './Home.jsx'
 import WakingMindOfGod from './WakingMindOfGod.jsx'
 import HowToBuildWebApp from './HowToBuildWebApp.jsx'
 import EpistemicTraps from './EpistemicTraps.jsx'
+import APEMO from './APEMO.jsx'
+import WorkflowPerturb from './WorkflowPerturb.jsx'
+import StandardizedEval from './StandardizedEval.jsx'
+import Logitext from './Logitext.jsx'
+import Propensities from './Propensities.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -14,6 +19,11 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path="/waking-the-mind-of-god" element={<WakingMindOfGod />} />
         <Route path="/how-to-build-a-web-app" element={<HowToBuildWebApp />} />
         <Route path="/epistemic-traps" element={<EpistemicTraps />} />
+        <Route path="/apemo" element={<APEMO />} />
+        <Route path="/workflowperturb" element={<WorkflowPerturb />} />
+        <Route path="/standardized-eval" element={<StandardizedEval />} />
+        <Route path="/logitext" element={<Logitext />} />
+        <Route path="/propensities" element={<Propensities />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
